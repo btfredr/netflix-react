536b06d3e6fdca3ca8c7c45e05b4bd9a586bc708
Fredrik <fredrikf2000@gmail.com>
2023-03-27 17:47:33 +0200

    removed bloat and added sass

---
+++ src/components/Homescreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Homescreen = () => {
+  return (
+    <div className="homescreen">
+      <Nav />
+    </div>
+  );
+};
+
+export default Homescreen;
