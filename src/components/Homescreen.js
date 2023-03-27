fea940fc7a5987e6cfb24ab7241d0f211f30f71c
Fredrik <fredrikf2000@gmail.com>
2023-03-27 18:14:38 +0200

    started making banner component

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 const Homescreen = () => {
   return (
     <div className="homescreen">
       <Nav />
+      <Banner />
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,7 @@ import avatar from "../assets/avatar.png";
 const Nav = () => {
   const [show, handleShow] = useState(false);
 
+  // function to make nav background black when user scrolls down the page
   const transitionNavbar = () => {
     if (window.showY > 100) {
       handleShow(true);
@@ -13,13 +14,14 @@ const Nav = () => {
     }
   };
 
+  // adding event listener to navbar when user scrolls, and adding the apprapriate className
   useEffect(() => {
     window.addEventListener("scroll", transitionNavbar);
     return () => window.removeEventListener("scroll", transitionNavbar);
   }, []);
 
   return (
-    <div className="nav nav__black">
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__content">
         <img className="nav__logo" src={logo} alt="Netflix logo" />
         <img className="nav__avatar" src={avatar} alt="User avatar" />
