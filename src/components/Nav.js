35727f9d2bb3024141cef7c4af90aa990dc5153d
Fredrik <fredrikf2000@gmail.com>
2023-03-27 17:53:55 +0200

    working on navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,16 @@
 import React from "react";
+import logo from "../assets/netflix.png";
+import avatar from "../assets/avatar.png";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      <div className="nav__content">
+        <img className="nav__logo" src={logo} alt="Netflix logo" />
+        <img className="nav__avatar" src={avatar} alt="User avatar" />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
