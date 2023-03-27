a63a7184a62bb0054f3aa76568bc3b581494f471
Fredrik <fredrikf2000@gmail.com>
2023-03-27 19:25:13 +0200

    fetching movies and info

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import bannerblack from "../assets/bannerblack.png";
+import requests from "../utils/Requests";
+import axios from "../utils/axios";
 
 const Banner = () => {
+  // state to set movie to be displayed inside banner at top of site
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    // fetching a movie from the database to display
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          // choosing random number to find a random movie to display from the selected request
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   // adding the option to "show more" if item description is longer than a set amount of characters
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
@@ -12,12 +34,14 @@ const Banner = () => {
       className="banner"
       style={{
         backgroudSize: "cover",
-        backgroundImage: `url(${bannerblack})`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__content">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
