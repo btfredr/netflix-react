94f20a0c35922fe1d05ca4158b9901abc0f0a2a8
Fredrik <fredrikf2000@gmail.com>
2023-03-27 18:38:47 +0200

    styling banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,27 @@
 import React from "react";
+import bannerblack from "../assets/bannerblack.png";
 
 const Banner = () => {
-  return;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroudSize: "cover",
+        backgroundImage: `url(${bannerblack})`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__content">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">This is a test description</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 };
 
 export default Banner;
