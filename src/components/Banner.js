1c7c9b9ae9f376bf47f0dd034e4b999df82f5acf
Fredrik <fredrikf2000@gmail.com>
2023-03-27 18:51:01 +0200

    wrote truncate function in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,11 @@ import React from "react";
 import bannerblack from "../assets/bannerblack.png";
 
 const Banner = () => {
+  // adding the option to "show more" if item description is longer than a set amount of characters
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
   return (
     <header
       className="banner"
@@ -17,7 +22,15 @@ const Banner = () => {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">This is a test description</h1>
+        <h1 className="banner__description">
+          {truncate(
+            `This is a test description
+            This is a test descriptionThis is a test descriptionT
+            his is a test descriptionThis is a test descriptionThis i
+            s a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`,
+            150
+          )}
+        </h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
