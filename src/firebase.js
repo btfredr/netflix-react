27474ddc0295c4fbafe442278d43a1b1e061c4d5
Fredrik <fredrikf2000@gmail.com>
2023-03-27 17:09:25 +0200

    project init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmp2Zy6pE0vUvqAB2c7Bx3yVXwl_YxNVE",
+  authDomain: "netflix-clone-c7b4e.firebaseapp.com",
+  projectId: "netflix-clone-c7b4e",
+  storageBucket: "netflix-clone-c7b4e.appspot.com",
+  messagingSenderId: "1098989822317",
+  appId: "1:1098989822317:web:fa4a5263ef6df9839c3a64",
+};
