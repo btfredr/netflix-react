b797063c049f2fa6661c286624df75765c0ad799
Fredrik <fredrikf2000@gmail.com>
2023-03-29 17:08:24 +0200

    adding sign up functionality

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,15 +1,28 @@
 import React from "react";
 
 const SignUp = () => {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signup">
       <form className="signup__form">
         <h1>Sign In</h1>
         <input placeholder="Email" type="email" />
         <input placeholder="Password" type="password" />
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={signIn}>
+          Submit
+        </button>
         <h4>
-          New to Netflix? <span>Sign Up now.</span>
+          <span className="signup__gray">New to Netflix?</span>
+          <span className="signup__link" onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
