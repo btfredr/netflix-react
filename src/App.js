a98e6513982eafffeb4c5ef893b8dc762db23208
Fredrik <fredrikf2000@gmail.com>
2023-03-29 18:04:56 +0200

    added user authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import Homescreen from "./screens/Homescreen";
 import Login from "./screens/Login";
+import { auth } from "./utils/firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -23,6 +23,17 @@ const SignUp = () => {
 
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
