f09dc900917f7f824574b392acc5f17bdcff9918
Fredrik <fredrikf2000@gmail.com>
2023-03-30 13:43:18 +0200

    added logout functionality

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,6 +3,7 @@ import Nav from "../components/Nav";
 import avatar from "../assets/avatar.png";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
+import { auth } from "../utils/firebase";
 
 const Profile = () => {
   const user = useSelector(selectUser);
@@ -17,8 +18,18 @@ const Profile = () => {
         </div>
         <div className="profile__details">
           <h2>{user.email}</h2>
-          <div className="profile__plans"></div>
+          <div className="profile__plans">
+            <button
+              onClick={() => {
+                auth.signOut();
+              }}
+              className="profile__signout"
+            >
+              Sign Out
+            </button>
+          </div>
         </div>
+        llllll
       </div>
     </div>
   );
