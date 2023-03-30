c96805b18ad0e6b86c8c934570a0af11c9d4113a
Fredrik <fredrikf2000@gmail.com>
2023-03-30 11:14:41 +0200

    fixed typo in store.js to allow user auth

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import React, { useEffect } from "react";
 import Homescreen from "./screens/Homescreen";
 import Login from "./screens/Login";
 import { auth } from "./utils/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
+        // Logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
+        // Logged out
+        dispatch(logout);
       }
     });
 
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -5,7 +5,6 @@ export const userSlice = createSlice({
   initialState: {
     user: null,
   },
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
@@ -18,9 +17,6 @@ export const userSlice = createSlice({
 
 export const { login, logout } = userSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
